b0aa01463ae2f4eea67a4bb8636ce79818913a4c
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-11-02 17:25:59 +0000

    python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,74 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+import neopixel
+class HCSR04:
+    # this class abstracts out the functionality of the HC-SR04 and
+    #   returns distance in mm
+    # Trig: pin 1
+    # Echo: pin 2
+    def __init__(self, tpin=pin1, epin=pin2, spin=pin13):
+        self.trigger_pin = tpin
+        self.echo_pin = epin
+        self.sclk_pin = spin
 
+    def distance_mm(self):
+        spi.init(
+            baudrate=125000,
+            sclk=self.sclk_pin,
+            mosi=self.trigger_pin,
+            miso=self.echo_pin,
+        )
+        pre = 0
+        post = 0
+        k = -1
+        length = 500
+        resp = bytearray(length)
+        resp[0] = 0xFF
+        spi.write_readinto(resp, resp)
+        # find first non zero value
+        try:
+            i, value = next((ind, v) for ind, v in enumerate(resp) if v)
+        except StopIteration:
+            i = -1
+        if i > 0:
+            pre = bin(value).count("1")
+            # find first non full high value afterwards
+            try:
+                k, value = next(
+                    (ind, v)
+                    for ind, v in enumerate(resp[i : length - 2])
+                    if resp[i + ind + 1] == 0
+                )
+                post = bin(value).count("1") if k else 0
+                k = k + i
+            except StopIteration:
+                i = -1
+        dist = -1 if i < 0 else round(((pre + (k - i) * 8.0 + post) * 8 * 0.172) / 2)
+        return dist
 
-display.scroll("Hello, World!")
+# variables
+sonar = HCSR04()
+distance_to_object = 0
+sonar.distance_mm = sonar.distance_mm() / 10
+
+# cleanup
+np = neopixel.NeoPixel(pin16, 4)
+np[0] = (0, 0, 0)
+np[1] = (0, 0, 0)
+np[2] = (0, 0, 0)
+np[3] = (0, 0, 0)
+np.show()
+
+while sonar.distance_mm() < 10:
+    np[0] = (1, 0, 0)
+    np[1] = (1, 0, 0)
+    np[2] = (1, 0, 0)
+    np[3] = (1, 0, 0)
+    np.show()
+else:
+    np[0] = (1, 0, 0)
+    np[1] = (1, 0, 0)
+    np[2] = (1, 0, 0)
+    np[3] = (1, 0, 0)
+    np.show()
